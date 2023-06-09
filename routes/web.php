196fd009be2019f24b21bfcfe037eb46b49b1340
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2023-06-09 21:32:51 +0700

    Resolve routes.php conflicts

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReserveController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\Admin\Auth\LoginController as AdminLoginController;
+use App\Http\Controllers\Admin\DashboardController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +28,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('login', [AdminLoginController::class, 'showLoginForm'])->name('login');
+    //protected routes
+    Route::middleware(['auth'])->group(function() {
+        Route::get('/', DashboardController::class)->name('home');
+        Route::get('/dashboard', DashboardController::class)->name('dashboard');
+        Route::resource('room',RoomController::class);
+    });
+    
+});
