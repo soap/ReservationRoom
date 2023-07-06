c0dea38b7f004ca3473fb607302e215cd05654a5
akichi139 <supphakit.c@ku.th>
2023-07-06 15:25:26 +0700

    set without vite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use SebastianBergmann\Type\VoidType;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void{
+        parent::setUp();
+        $this->withoutVite();
+    }
 }
